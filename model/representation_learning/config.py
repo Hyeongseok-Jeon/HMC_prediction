c911cd5002ddabceddfe2e3c763daf3854711db5
hyeongseokjeon <521hsgugu@kaist.ac.kr>
2022-02-09 03:32:47 +0900

    asdf

--- model/representation_learning/config.py
+++ model/representation_learning/config.py
@@ -9,10 +9,11 @@ config = dict()
 config["GPU_id"] = 0
 
 ''' training '''
-config["batch_size"] = 1
+config["batch_size"] = 100
 config["epoch"] = 50
 
 ''' data '''
 config["data_dir"] = data_dir
 config["occlusion_rate"] = 0.2
 config["splicing_num"] = 128
+config["FOV"] = 30
